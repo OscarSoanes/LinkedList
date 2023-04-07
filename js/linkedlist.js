e5cbc5bca48c3b087991c6fdaa9bd448278bc585
Oscar Soanes <oscarsoanes@gmail.com>
2023-04-07 17:27:09 +0100

    Prepend build inside the Linked List

--- js/linkedlist.js
+++ js/linkedlist.js
@@ -5,6 +5,10 @@ export class LinkedList {
     this.list = list;
   }
 
+  /**
+   * Appends a value to the end of the Linked List.
+   * @param {*} value, value to be appended to List
+   */
   append(value) {
     const newNode = new Node(value);
 
@@ -13,6 +17,7 @@ export class LinkedList {
       return;
     }
 
+    // Gets the current Node, loops until it finds null.
     let node = this.list;
     while (node.nextNode !== null) {
       node = node.nextNode;
@@ -21,7 +26,23 @@ export class LinkedList {
     node.nextNode = newNode;
   }
 
+  /**
+   * Prepends a value to the start of the Linked List.
+   * @param {*} value, value to be prepended to the List
+   */
+  prepend(value) {
+    const newNode = new Node(value);
+
+    if (this.list === null) {
+      this.list = newNode;
+      return;
+    }
+
+    newNode.nextNode = this.list;
+    this.list = newNode;
+  }
+
   display() {
-    console.log(this.list);
+    return this.list;
   }
 }
